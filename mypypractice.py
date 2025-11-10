1d7a8e23a152f43989f645322279c125ebae401e
NagaRepaka-cyber <venkatash.repaka@gmail.com>
2025-11-09 21:20:11 -0600

    moving pythonfiles to harinipython

---
+++ mypypractice.py
@@ -0,0 +1,24 @@
+x=11
+while x>10:
+    x=x+1
+    print("adding 1")
+    if x==15:
+        print(x)
+        break
+
+y=0
+while y>=0:
+    y=y+1
+    if y//2 == 0:
+        print(y,"even")
+    elif y//2 == 1:
+        print(y,"odd")
+    else:
+        break
+    print(y)
+    
+    
+
+
+
+    
